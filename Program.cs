9a0769077bd3cb354fa3eb42b5514f4c01d65754
Joshua Abbate <joshuajka@hotmail.com>
2023-10-12 17:53:12 -0300

    implementando metodos

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Nokia nokia = new Nokia("135456468", "oracle", "fsdanhkjcdas231jb3124v", 4);
+        Iphone iphone = new Iphone("13545643468", "5SE", "fsdanhkjcda421324jb3124v", 16);
+
+        nokia.InstalarAplicativo("Waze");
+        iphone.InstalarAplicativo("Uber");
+    }
+}
